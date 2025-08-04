6fbb9c38d2e99fdd4c6b92d155956a7e9278ff2c
Labtory-82 <chnny9@gmail.com>
2025-08-04 12:42:17 +0900

    커밋

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,9 +11,8 @@ public class App {
         //명언 배열
         WiseSaying[] wiseSayings = new WiseSaying[999];
 
-        //number는 명언 번호, tmpNumber는 나중에 임시로 사용할 변수
+        //명언 번호
         int number = -1;
-        int tmpNumber = 0;
 
         System.out.println("== 명언 앱 ==");
         //"종료" 명령이 입력될 때까지 반복
@@ -28,7 +27,7 @@ public class App {
                 number++;
 
                 WiseSaying wiseSaying = new WiseSaying();
-                wiseSaying.id = number;
+                wiseSaying.id = number+1;
                 System.out.print("명언 : ");
                 wiseSaying.content = sc.nextLine();
                 System.out.print("작가 : ");
@@ -40,53 +39,48 @@ public class App {
 
             //"목록"을 입력받았을 경우
             if (command.equals("목록")) {
-                tmpNumber = number;
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
 
                 //마지막 번호의 명언부터 첫 번호의 명언까지 출력
-                while (tmpNumber + 1 > 0) {
-
-                    if (wiseSayings[tmpNumber] == null) {
-                        tmpNumber--;
-                        continue;
-                    }
-
-                    System.out.println((tmpNumber + 1) + " / " + wiseSayings[tmpNumber].author + " / " + wiseSayings[tmpNumber].content);
-                    tmpNumber--;
+                for(int i = number; i >= 0; i--) {
+                    if (wiseSayings[i] == null) continue;
+                    System.out.println(wiseSayings[i].id+ " / " + wiseSayings[i].author + " / " + wiseSayings[i].content);
                 }
             }
 
             //삭제 명령을 입력받았을 경우. 명언 id가 다르게 들어오기 때문에 정규표현식으로 검사
             if (command.matches("^삭제\\?id=\\d+$")) {
-                tmpNumber = Integer.parseInt(command.substring(6)) - 1;
+                //입력값에서 id숫자 추출
+                int id = Integer.parseInt(command.substring(6)) - 1;
 
                 //명언 최대 개수를 초과한 숫자를 입력받았을 경우 또는 존재하지 않는 명언일 경우 예외 처리
-                if (tmpNumber > 999 || (wiseSayings[tmpNumber] == null)) {
-                    System.out.println((tmpNumber+1) + "번 명언은 존재하지 않습니다.");
+                if (id > 999 || (wiseSayings[id] == null)) {
+                    System.out.println((id+1) + "번 명언은 존재하지 않습니다.");
                 }
                 //예외가 발생하지 않는다면 정상적으로 삭제 처리
                 else {
-                    wiseSayings[tmpNumber] = null;
-                    System.out.println((tmpNumber+1) + "번 명언이 삭제되었습니다.");
+                    wiseSayings[id] = null;
+                    System.out.println((id+1) + "번 명언이 삭제되었습니다.");
                 }
             }
 
             //수정 명령을 입력받았을 경우. 역시 정규표현식으로 검사
             if (command.matches("^수정\\?id=\\d+$")) {
-                tmpNumber = Integer.parseInt(command.substring(6)) - 1;
+                int id = Integer.parseInt(command.substring(6)) - 1;
+
                 //삭제 동작과 같은 예외 처리
-                if (tmpNumber > 999 || wiseSayings[tmpNumber] == null) {
-                    System.out.println((tmpNumber+1) + "번 명언은 존재하지 않습니다.");
+                if (id > 999 || wiseSayings[id] == null) {
+                    System.out.println((id+1) + "번 명언은 존재하지 않습니다.");
                 }
                 //예외가 발생하지 않는다면 정상적으로 수정 처리
                 else {
-                    System.out.println("명언(기존) : " + wiseSayings[tmpNumber].content);
+                    System.out.println("명언(기존) : " + wiseSayings[id].content);
                     System.out.print("명언 : ");
-                    wiseSayings[tmpNumber].content = sc.nextLine();
-                    System.out.println("작가(기존) : " + wiseSayings[tmpNumber].author);
+                    wiseSayings[id].content = sc.nextLine();
+                    System.out.println("작가(기존) : " + wiseSayings[id].author);
                     System.out.print("작가 : ");
-                    wiseSayings[tmpNumber].author = sc.nextLine();
+                    wiseSayings[id].author = sc.nextLine();
                 }
             }
         }
